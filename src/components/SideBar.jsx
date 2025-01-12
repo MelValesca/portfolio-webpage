7a2ddb7e0b33067c41711fea6e6e398c77884b3f
Melissa Valesca Vallee <valesca@Melissas-MacBook-Air.local>
2025-01-12 11:26:16 -0500

    create SideBar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "../styles/SideBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+const SideBar = ({ categories }) => {
+  const [expandedCategories, setExpandedCategories] = useState(new Set());
+
+  const toggleCategory = (index) => {
+    setExpandedCategories((prev) => {
+      const newExpandedCategories = new Set(prev);
+      if (newExpandedCategories.has(index)) {
+        newExpandedCategories.delete(index);
+      } else {
+        newExpandedCategories.add(index);
+      }
+      return newExpandedCategories;
+    });
+  };
+
+  return (
+    <div className="sideBarWrap">
+      {categories.map((category, index) => (
+        <div className="categoryWrap" key={index}>
+          <div className="categoryHeader" onClick={() => toggleCategory(index)}>
+            <span>{category.name}</span>
+            <FontAwesomeIcon
+              icon={expandedCategories.has(index) ? faChevronUp : faChevronDown}
+              className="sideBarArrow"
+            />
+          </div>
+
+          {expandedCategories.has(index) && (
+            <div className="categoryLinks">
+              {category.links.map((link, i) => (
+                <div key={i} className="sideBarLink">
+                  {link}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SideBar;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import SideBar from "../components/SideBar";
 
 const Projects = () => {
+  const divisions = [
+    {
+      name: "Web apps",
+      links: ["\ttest1", "\ttest2", "\ttest3"],
+    },
+    {
+      name: "Desktop apps",
+      links: ["\tCurler (GUI)"],
+    },
+    {
+      name: "Scripts",
+      links: ["\tShell/Bash", "\tPython", "\tC"],
+    },
+  ];
 
-    return (
-        <h1>test</h1>
-    )
+  return <SideBar categories={divisions}></SideBar>;
 };
 
 export default Projects;
